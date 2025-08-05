a681f28ac4d7473b11cd9e19caa63090632e3d28
kentaro.aoki <aoken0714@users.noreply.github.com>
2025-08-05 11:00:48 +0900

    add FirstOfArray

---
+++ src/questions/easy/FirstOfArray.ts
@@ -0,0 +1,18 @@
+import type { Equal, Expect } from '@/utils/test-types';
+
+// https://github.com/type-challenges/type-challenges/blob/main/questions/00014-easy-first/README.md
+type First<T extends unknown[]> = T extends [] ? never : T[0]
+
+type cases = [
+  Expect<Equal<First<[3, 2, 1]>, 3>>,
+  Expect<Equal<First<[() => 123, { a: string }]>, () => 123>>,
+  Expect<Equal<First<[]>, never>>,
+  Expect<Equal<First<[undefined]>, undefined>>,
+]
+
+type errors = [
+  // @ts-expect-error
+  First<'notArray'>,
+  // @ts-expect-error
+  First<{ 0: 'arrayLike' }>,
+]
