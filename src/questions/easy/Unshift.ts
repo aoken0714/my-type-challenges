044da1445df875fd3d06e0fe2565895c30b1bc7e
kentaro.aoki <aoken0714@users.noreply.github.com>
2025-08-05 16:13:50 +0900

    add Unshift

---
+++ src/questions/easy/Unshift.ts
@@ -0,0 +1,10 @@
+import type { Equal, Expect } from '@/utils/test-types';
+
+// https://github.com/type-challenges/type-challenges/blob/main/questions/03060-easy-unshift/README.md
+type Unshift<T extends unknown[], U> = [U, ...T];
+
+type cases = [
+  Expect<Equal<Unshift<[], 1>, [1]>>,
+  Expect<Equal<Unshift<[1, 2], 0>, [0, 1, 2]>>,
+  Expect<Equal<Unshift<['1', 2, '3'], boolean>, [boolean, '1', 2, '3']>>,
+];
