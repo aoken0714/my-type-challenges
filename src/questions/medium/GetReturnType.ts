8c2a2a5a90f6784d87d8ca60f39b9769770dd54b
kentaro.aoki <aoken0714@users.noreply.github.com>
2025-08-07 11:29:27 +0900

    add GetReturnType

---
+++ src/questions/medium/GetReturnType.ts
@@ -0,0 +1,25 @@
+import type { Equal, Expect } from '@/utils/test-types';
+
+// https://github.com/type-challenges/type-challenges/blob/main/questions/00002-medium-return-type/README.md
+type MyReturnType<T extends Function> = T extends (...args: any[]) => infer R
+  ? R
+  : never;
+
+type cases = [
+  Expect<Equal<string, MyReturnType<() => string>>>,
+  Expect<Equal<123, MyReturnType<() => 123>>>,
+  Expect<Equal<ComplexObject, MyReturnType<() => ComplexObject>>>,
+  Expect<Equal<Promise<boolean>, MyReturnType<() => Promise<boolean>>>>,
+  Expect<Equal<() => 'foo', MyReturnType<() => () => 'foo'>>>,
+  Expect<Equal<1 | 2, MyReturnType<typeof fn>>>,
+  Expect<Equal<1 | 2, MyReturnType<typeof fn1>>>,
+];
+
+type ComplexObject = {
+  a: [12, 'foo'];
+  bar: 'hello';
+  prev(): number;
+};
+
+const fn = (v: boolean) => (v ? 1 : 2);
+const fn1 = (v: boolean, w: any) => (v ? 1 : 2);
