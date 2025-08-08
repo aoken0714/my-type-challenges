24abce73a0baf89150d344c28680789e3997e0a0
kentaro.aoki <aoken0714@users.noreply.github.com>
2025-08-08 12:44:07 +0900

    add Permutation

---
+++ src/questions/medium/Permutation.ts
@@ -0,0 +1,36 @@
+import type { Equal, Expect } from '@/utils/test-types';
+
+// https://github.com/type-challenges/type-challenges/blob/main/questions/00296-medium-permutation/README.ja.md
+type Permutation<T, K = T> = [T] extends [never]
+  ? []
+  : K extends K
+    ? [K, ...Permutation<Exclude<T, K>>]
+    : never;
+
+type cases = [
+  Expect<Equal<Permutation<'A'>, ['A']>>,
+  Expect<
+    Equal<
+      Permutation<'A' | 'B' | 'C'>,
+      | ['A', 'B', 'C']
+      | ['A', 'C', 'B']
+      | ['B', 'A', 'C']
+      | ['B', 'C', 'A']
+      | ['C', 'A', 'B']
+      | ['C', 'B', 'A']
+    >
+  >,
+  Expect<
+    Equal<
+      Permutation<'B' | 'A' | 'C'>,
+      | ['A', 'B', 'C']
+      | ['A', 'C', 'B']
+      | ['B', 'A', 'C']
+      | ['B', 'C', 'A']
+      | ['C', 'A', 'B']
+      | ['C', 'B', 'A']
+    >
+  >,
+  Expect<Equal<Permutation<boolean>, [false, true] | [true, false]>>,
+  Expect<Equal<Permutation<never>, []>>,
+];
