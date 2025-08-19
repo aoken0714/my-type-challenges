b1d485f23807aea9e51d68db40c84dd436d1bf5e
kentaro.aoki <aoken0714@users.noreply.github.com>
2025-08-19 15:23:22 +0900

    add isUnion

---
+++ src/questions/medium/isUnion.ts
@@ -0,0 +1,26 @@
+import type { Equal, Expect } from '@/utils/test-types';
+
+// https://github.com/type-challenges/type-challenges/blob/main/questions/01097-medium-isunion/README.md
+type IsUnion<T, U = T> = [T] extends [never]
+  ? false
+  : T extends any
+    ? [U] extends [T]
+      ? false
+      : true
+    : never;
+
+type cases = [
+  Expect<Equal<IsUnion<string>, false>>,
+  Expect<Equal<IsUnion<string | number>, true>>,
+  Expect<Equal<IsUnion<'a' | 'b' | 'c' | 'd'>, true>>,
+  Expect<Equal<IsUnion<undefined | null | void | ''>, true>>,
+  Expect<Equal<IsUnion<{ a: string } | { a: number }>, true>>,
+  Expect<Equal<IsUnion<{ a: string | number }>, false>>,
+  Expect<Equal<IsUnion<[string | number]>, false>>,
+  // Cases where T resolves to a non-union type.
+  Expect<Equal<IsUnion<string | never>, false>>,
+  Expect<Equal<IsUnion<string | unknown>, false>>,
+  Expect<Equal<IsUnion<string | any>, false>>,
+  Expect<Equal<IsUnion<string | 'a'>, false>>,
+  Expect<Equal<IsUnion<never>, false>>,
+];
