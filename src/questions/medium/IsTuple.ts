65e77b9ba4ddb49c97a760b1c0bb6276d0ab4d38
kentaro.aoki <aoken0714@users.noreply.github.com>
2025-09-07 06:35:18 +0900

    add IsTuple

---
+++ src/questions/medium/IsTuple.ts
@@ -0,0 +1,18 @@
+import type { Equal, Expect } from '@/utils/test-types';
+
+type IsTuple<T> = [T] extends [never]
+  ? false
+  : T extends readonly unknown[]
+    ? number extends T['length']
+      ? false
+      : true
+    : false;
+
+type cases = [
+  Expect<Equal<IsTuple<[]>, true>>,
+  Expect<Equal<IsTuple<[number]>, true>>,
+  Expect<Equal<IsTuple<readonly [1]>, true>>,
+  Expect<Equal<IsTuple<{ length: 1 }>, false>>,
+  Expect<Equal<IsTuple<number[]>, false>>,
+  Expect<Equal<IsTuple<never>, false>>,
+];
