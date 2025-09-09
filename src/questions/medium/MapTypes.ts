b47a803b99feb6526ce56d3386e10238d7da3ebd
kentaro.aoki <aoken0714@users.noreply.github.com>
2025-09-09 22:25:52 +0900

    add MapTypes

---
+++ src/questions/medium/MapTypes.ts
@@ -0,0 +1,77 @@
+import type { Equal, Expect } from '@/utils/test-types';
+
+// https://github.com/type-challenges/type-challenges/blob/main/questions/05821-medium-maptypes/README.md
+type FindMapping<T, R> = R extends { mapFrom: infer From; mapTo: infer To }
+  ? T extends From
+    ? To
+    : never
+  : never;
+
+type MapTypes<T, R> = {
+  [K in keyof T]: FindMapping<T[K], R> extends never
+    ? T[K]
+    : FindMapping<T[K], R>;
+};
+
+type cases = [
+  Expect<
+    Equal<
+      MapTypes<{ stringToArray: string }, { mapFrom: string; mapTo: [] }>,
+      { stringToArray: [] }
+    >
+  >,
+  Expect<
+    Equal<
+      MapTypes<{ stringToNumber: string }, { mapFrom: string; mapTo: number }>,
+      { stringToNumber: number }
+    >
+  >,
+  Expect<
+    Equal<
+      MapTypes<
+        { stringToNumber: string; skipParsingMe: boolean },
+        { mapFrom: string; mapTo: number }
+      >,
+      { stringToNumber: number; skipParsingMe: boolean }
+    >
+  >,
+  Expect<
+    Equal<
+      MapTypes<
+        { date: string },
+        { mapFrom: string; mapTo: Date } | { mapFrom: string; mapTo: null }
+      >,
+      { date: null | Date }
+    >
+  >,
+  Expect<
+    Equal<
+      MapTypes<{ date: string }, { mapFrom: string; mapTo: Date | null }>,
+      { date: null | Date }
+    >
+  >,
+  Expect<
+    Equal<
+      MapTypes<
+        { fields: Record<string, boolean> },
+        { mapFrom: Record<string, boolean>; mapTo: string[] }
+      >,
+      { fields: string[] }
+    >
+  >,
+  Expect<
+    Equal<
+      MapTypes<{ name: string }, { mapFrom: boolean; mapTo: never }>,
+      { name: string }
+    >
+  >,
+  Expect<
+    Equal<
+      MapTypes<
+        { name: string; date: Date },
+        { mapFrom: string; mapTo: boolean } | { mapFrom: Date; mapTo: string }
+      >,
+      { name: boolean; date: string }
+    >
+  >,
+];
