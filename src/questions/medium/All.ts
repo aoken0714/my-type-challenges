2977013fe3c14ef7ba5a078601b0644ef9f354f2
kentaro.aoki <aoken0714@users.noreply.github.com>
2025-09-14 22:55:34 +0900

    add All

---
+++ src/questions/medium/All.ts
@@ -0,0 +1,29 @@
+import type { Equal, Expect } from '@/utils/test-types';
+
+// https://github.com/type-challenges/type-challenges/blob/main/questions/18142-medium-all/README.md
+type All<T extends readonly unknown[], U> = T extends readonly [
+  infer First,
+  ...infer Rest,
+]
+  ? Equal<First, U> extends true
+    ? All<Rest, U>
+    : false
+  : true;
+
+type cases = [
+  Expect<Equal<All<[1, 1, 1], 1>, true>>,
+  Expect<Equal<All<[1, 1, 2], 1>, false>>,
+  Expect<Equal<All<['1', '1', '1'], '1'>, true>>,
+  Expect<Equal<All<['1', '1', '1'], 1>, false>>,
+  Expect<Equal<All<[number, number, number], number>, true>>,
+  Expect<Equal<All<[number, number, string], number>, false>>,
+  Expect<Equal<All<[null, null, null], null>, true>>,
+  Expect<Equal<All<[[1], [1], [1]], [1]>, true>>,
+  Expect<Equal<All<[{}, {}, {}], {}>, true>>,
+  Expect<Equal<All<[never], never>, true>>,
+  Expect<Equal<All<[any], any>, true>>,
+  Expect<Equal<All<[unknown], unknown>, true>>,
+  Expect<Equal<All<[any], unknown>, false>>,
+  Expect<Equal<All<[unknown], any>, false>>,
+  Expect<Equal<All<[1, 1, 2], 1 | 2>, false>>,
+];
