a2271c842089a89364f4c710791bf801997e4966
kentaro.aoki <aoken0714@users.noreply.github.com>
2025-09-27 00:14:34 +0900

    add CamelCase

---
+++ src/questions/hard/CamelCase.ts
@@ -0,0 +1,24 @@
+import type { Equal, Expect } from '@/utils/test-types';
+
+// https://github.com/type-challenges/type-challenges/blob/main/questions/00114-hard-camelcase/README.md
+type CamelCase<S extends string> = S extends `${infer L}_${infer R1}${infer R2}`
+  ? Uppercase<R1> extends Lowercase<R1>
+    ? `${Lowercase<L>}_${CamelCase<`${R1}${R2}`>}`
+    : `${Lowercase<L>}${Uppercase<R1>}${CamelCase<R2>}`
+  : Lowercase<S>;
+
+type cases = [
+  Expect<Equal<CamelCase<'foobar'>, 'foobar'>>,
+  Expect<Equal<CamelCase<'FOOBAR'>, 'foobar'>>,
+  Expect<Equal<CamelCase<'foo_bar'>, 'fooBar'>>,
+  Expect<Equal<CamelCase<'foo__bar'>, 'foo_Bar'>>,
+  Expect<Equal<CamelCase<'foo_$bar'>, 'foo_$bar'>>,
+  Expect<Equal<CamelCase<'foo_bar_'>, 'fooBar_'>>,
+  Expect<Equal<CamelCase<'foo_bar__'>, 'fooBar__'>>,
+  Expect<Equal<CamelCase<'foo_bar_$'>, 'fooBar_$'>>,
+  Expect<Equal<CamelCase<'foo_bar_hello_world'>, 'fooBarHelloWorld'>>,
+  Expect<Equal<CamelCase<'HELLO_WORLD_WITH_TYPES'>, 'helloWorldWithTypes'>>,
+  Expect<Equal<CamelCase<'-'>, '-'>>,
+  Expect<Equal<CamelCase<''>, ''>>,
+  Expect<Equal<CamelCase<'😎'>, '😎'>>,
+];
